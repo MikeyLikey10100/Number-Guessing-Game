1cad1405d89ed4fcdde822f536fd1e36e580ab97
michael.Nguyen <michael.Nguyen@MU-C1-18.mullen.pvt.k12.co.us>
2019-08-22 10:57:27 -0600

    initial commit

---
+++ Number guessing game/src/numberGuessingGame.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+public class numberGuessingGame
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+			
+					{
+			
+					
+					
+					
+					}
+				int randomNumber = (int) (Math.random() *20 + 1);
+		
+
+			{
+					{		
+				Scanner userImput = new Scanner(System.in);
+				System.out.println("Guess a number between 1 and 15");
+				
+			int guess = userImput.nextInt(); 
+			boolean stillGuessing;
+			
+			while(stillGuessing)
+				{
+		if ( guess < randomNumber )
+			{
+				System.out.println("your number is too low");
+				
+			}
+				
+			
+		
+		else if (guess > randomNumber)
+			{
+				
+				System.out.println("Your number is too high");
+				
+			}
+					
+				}
+					
+			else {
+			System.out.println("your correct. please play again");
+	
+		
+		}
+					}
+					
+			
+					}
+			
+			
+	
+	}
+	}
+				
+				
+				
+				
+				
+			
+
+	
